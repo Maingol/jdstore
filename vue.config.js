2640d2c2d8e5bf2dc25537d1ec51112de3e2fc60
Zhang Jiaming <zhangjiaming1@le.com>
2020-11-02 12:17:47 +0800

    实现首页内容定制
    Signed-off-by: Zhang Jiaming <zhangjiaming1@le.com>

--- vue.config.js
+++ vue.config.js
@@ -19,11 +19,23 @@ module.exports = {
         nprogress: 'NProgress',
         'vue-quill-editor': 'VueQuillEditor'
       })
+
+      // 向html插件中增加一个属性，用于判断是否向页面渲染特定内容
+      config.plugin('html').tap(args => {
+        args[0].isProd = true
+        return args
+      })
     })
 
     // 产品开发阶段
     config.when(process.env.NODE_ENV === 'development', config => {
       config.entry('app').clear().add('./src/main-dev.js')
+
+      // 向html插件中增加一个属性，用于判断是否向页面渲染特定内容
+      config.plugin('html').tap(args => {
+        args[0].isProd = false
+        return args
+      })
     })
   }
 }
