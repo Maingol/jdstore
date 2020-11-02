cc9cd227d38d62884ff6d4476943a09bcf121fa8
Zhang Jiaming <zhangjiaming1@le.com>
2020-11-02 11:19:27 +0800

    页面开启loading效果
    Signed-off-by: Zhang Jiaming <zhangjiaming1@le.com>

--- src/main.js
+++ src/main.js
@@ -21,6 +21,10 @@ import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
 
+// (用于实现打开页面时的loading效果)导入NProgress包对应的js和css
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 // 配置axios请求的基准路径
 axios.defaults.baseURL = 'http://127.0.0.1:8700/api/private/v1/'
 
@@ -28,6 +32,8 @@ axios.defaults.baseURL = 'http://127.0.0.1:8700/api/private/v1/'
 // 参考https://www.cnblogs.com/Jack-cx/p/12081745.html
 // https://learnku.com/articles/37226 (重要)
 axios.interceptors.request.use(config => {
+  // 开启页面loading效果
+  NProgress.start()
   // 打印出来的是相对路径 >>login
   // console.log(config.url)
   // 这里做不做判断其实都可以，因为后端是不会对登陆请求做鉴权操作的
@@ -43,26 +49,27 @@ axios.interceptors.request.use(config => {
 
 // 设置响应拦截器，直接拿出返回对象中的data数据
 // axios.interceptors.response.use(res => (res.data))
-axios.interceptors.response.use(
-  response => {
-    // 未登录或会话已过期
-    if (response.data.meta !== undefined && response.data.meta.status === 401) {
-      // 重定向到登录页
-      router.push('/login')
-      Vue.prototype.$message.error('您没有登陆或登陆时效已过期,请重新登陆')
-    }
-    if (response.data.meta !== undefined && response.data.meta.status === 403) {
-      // 用户无权访问该资源
-      Vue.prototype.$message.error('权限不足')
-    }
-    return response.data
-  },
-  error => {
-    if (error.response.status === 500) {
-      // 服务端异常
-    }
-    return Promise.reject(error) // 返回接口返回的错误信息
+axios.interceptors.response.use(response => {
+  // 隐藏页面loading效果
+  NProgress.done()
+  // 未登录或会话已过期
+  if (response.data.meta !== undefined && response.data.meta.status === 401) {
+    // 重定向到登录页
+    router.push('/login')
+    Vue.prototype.$message.error('您没有登陆或登陆时效已过期,请重新登陆')
   }
+  if (response.data.meta !== undefined && response.data.meta.status === 403) {
+    // 用户无权访问该资源
+    Vue.prototype.$message.error('权限不足')
+  }
+  return response.data
+},
+error => {
+  if (error.response.status === 500) {
+    // 服务端异常
+  }
+  return Promise.reject(error) // 返回接口返回的错误信息
+}
 )
 
 // 把axios包挂载到Vue的原型对象上，这样就只需导入一次axios包之后每个组件中就都可以发送axios请求了
